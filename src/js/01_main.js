fd7e119e1de8fb039b7c49ebbbdabc01a7f4efd8
Desginer123 <hotsru12@gmail.com>
2021-12-09 05:16:05 +0800

    some bug fixed 00:16

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -10,3 +10,34 @@ document.addEventListener('click', (e) => {
     menu.classList.remove('open')
   }
 })
+
+
+let users = document.querySelectorAll('.user');
+let userWrapper = document.querySelector('.user__wrapper')
+console.log(users);
+
+userWrapper.addEventListener('mouseover', (e) => {
+  users.forEach((user, key) => {
+    if(user.contains(e.target)) {
+      user.classList.add('active')
+    }
+    else {
+      user.classList.add('blured');
+      user.classList.remove('active')
+    }
+  });
+
+})
+
+userWrapper.addEventListener('mouseout', (e) => {
+  users.forEach((user, key) => {
+    if(user.contains(e.target)) {
+      user.classList.remove('active')
+    }
+    else {
+      user.classList.remove('blured');
+      user.classList.remove('active')
+    }
+  });
+
+})
